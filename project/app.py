a1bcec52671485c65d6550f4571ffc0716c0850a
frankbryce <jonnyjack7@gmail.com>
2021-12-22 22:31:55 -0500

    Stitch offset generation moved to server.
    Basic websocket installed to send data to client. Still jenky. I need to
    set up sending a message from the client to the server as well. That's
    next commit.

--- project/app.py
+++ project/app.py
@@ -1,17 +1,53 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+import json
+import numpy as np
+from time import sleep
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'TODO - wtf is this?'
+socketio = SocketIO(app)
 
+class NpEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        if isinstance(obj, np.floating):
+            return float(obj)
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return super(NpEncoder, self).default(obj)
+
+def jsEnc(data):
+    return json.dumps(data, cls=NpEncoder)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.route('/hello')
-def hello():
-    return render_template('hello.html')
+def sendRandom(cols, rows):
+    emit('json', {
+        'colOffsets': jsEnc(np.random.randint(2, size=(cols,))),
+        'rowOffsets': jsEnc(np.random.randint(2, size=(rows,))),
+    })
 
+# TODO how to send this from the client?
+@socketio.on('random')
+def onrandom():
+    print('send random')
+    sendRandom(20,20)
+
+
+@socketio.on('connect')
+def onconnect():
+    print('send random')
+    sendRandom(20,20)
+
+
+@socketio.on('disconnect')
+def test_disconnect():
+    print('Client disconnected')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True)
--- project/static/client.js
+++ project/static/client.js
@@ -1,23 +1,27 @@
-const numRows = 10
-const numCols = 10
+var socket = io();
+
+// for debugging
+socket.on('connect', function() {
+    console.log('socket is open');
+});
+socket.on('disconnect', function() {
+    console.log('socket is closed');
+});
+
 const stitchWeight = 2
 const stitchLen = 40
 
-d3.select('#glfield')
-    .attr('width', stitchLen * (numCols + 1))
-    .attr('height', stitchLen * (numRows + 1));
-
-function updateCols(selection, data) {
+function updateCols(selection, offsets, numRows) {
     let cols = selection
         .selectAll(".stitch-col")
-        .data(data);
+        .data(offsets);
     cols.exit().remove();
     group = cols.enter()
         .append('g')
         .attr("class", "stitch-col")
         .merge(cols);
     let stitches = group.selectAll("rect")
-        .data((d, i) => d3.map(d3.range(numRows/2), r => [i+1, d + 2*r]))
+        .data((offset, i) => d3.map(d3.range(numRows/2), r => [i+1, offset + 2*r]))
     stitches.exit().remove();
     stitches.enter()
         .append('rect')
@@ -29,17 +33,17 @@ function updateCols(selection, data) {
         .style("fill", "black");
 }
 
-function updateRows(selection, data) {
+function updateRows(selection, offsets, numCols) {
     let cols = selection
         .selectAll(".stitch-row")
-        .data(data);
+        .data(offsets);
     cols.exit().remove();
     group = cols.enter()
         .append('g')
         .attr("class", "stitch-row")
         .merge(cols);
     let stitches = group.selectAll("rect")
-        .data((d, i) => d3.map(d3.range(numCols/2), r => [d + 2*r, i+1]))
+        .data((offset, i) => d3.map(d3.range(numCols/2), r => [offset + 2*r, i+1]))
     stitches.exit().remove();
     stitches.enter()
         .append('rect')
@@ -51,21 +55,19 @@ function updateRows(selection, data) {
         .style("fill", "black");
 }
 
-colStitches = [0,1,0,1,0,1,0,1,0,1]
-rowStitches = [1,1,1,0,1,0,1,1,0,1]
-el = d3.select('#glfield')
-el.call(updateCols, colStitches);
-el.call(updateRows, rowStitches);
+socket.on('json', function(data) {
+    // read data from server
+    var colOffsets = JSON.parse(data.colOffsets);
+    var rowOffsets = JSON.parse(data.rowOffsets);
 
-for (let t=1;t<=10;t++) {
-    (new Promise((p) => setTimeout(p, 1000*t))).then(() => {
-        for (let i=0;i<colStitches.length;i++) {
-            colStitches[i] = 1 - colStitches[i];
-        }
-        for (let i=0;i<colStitches.length;i++) {
-            rowStitches[i] = 1 - rowStitches[i];
-        }
-        d3.select('#glfield').call(updateCols, colStitches);
-        d3.select('#glfield').call(updateRows, rowStitches);
-    });
-}
+    // update field size
+    let numCols = colOffsets.length;
+    let numRows = rowOffsets.length;
+    let el = d3.select('#glfield')
+        .attr('width', stitchLen * (numCols + 1))
+        .attr('height', stitchLen * (numRows + 1));
+
+    // update UI
+    el.call(updateCols, colOffsets, numRows);
+    el.call(updateRows, rowOffsets, numCols);
+});
