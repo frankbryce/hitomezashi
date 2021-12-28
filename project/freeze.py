8aad95d3457ed65f325aa4661eb27e5ea30770d8
frankbryce <jonnyjack7@gmail.com>
2021-12-28 13:41:18 -0500

    First attempt to create static version of app.

---
+++ project/freeze.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+from flask_frozen import Freezer
+from app import app
+
+app = Flask(__name__)
+freezer = Freezer(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    freezer.freeze()
--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@
 
 python=/usr/bin/python3
 
+$python project/freeze.py
 $python project/app.py
